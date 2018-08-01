e5908cc37ab7079448a76790a93eada7ac8c2504
nhunguyen2611 <mieutieunha@gmail.com>
2018-08-01 21:57:45 +0700

    Update NameComparator.java

--- 1-Comparator/NameComparator.java
+++ 1-Comparator/NameComparator.java
@@ -2,6 +2,7 @@ package comparator;
 
 import java.util.Comparator;
 
+// Sort ascending
 class NameComparator implements Comparator<Student>{  
 	public int compare(Student s1,Student s2){  
 		return s1.getName().compareTo(s2.getName());  
